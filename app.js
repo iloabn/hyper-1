126fb6f202eda28091765ae252bfc94e7a8afe83
iloabn <5629868+iloabn@users.noreply.github.com>
2025-03-26 13:15:23 +0100

    Repac of first two weeks tiiiiiiiny API + MongoDB

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
-import createError from 'http-errors';
 import express from 'express';
-import logger from 'morgan';
-
-import indexRouter from './routes/index.js';
+import cors from 'cors';
+import { MongoClient } from 'mongodb';
 
 const app = express();
+app.use(cors());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const mongoDBClient = new MongoClient("mongodb://localhost:27017");
 
-app.use('/', indexRouter);
+const connectDb = async () => {
+    await mongoDBClient.connect();
+    return mongoDBClient.db("recap").collection("moods");
+}
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+    res.send("Hello friend!");
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/moods', async (req, res) => {
+    const collection = await connectDb();
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send(res.locals.error);
+    const allMoods = await collection.find({}).toArray();
+
+    res.send(allMoods);
 });
 
-export default app;
+app.post('/mood/:moodName', async (req, res) => {
+
+    const { moodName } = req.params;
+
+    const collection = await connectDb();
+
+    const existingOne = await collection.findOne({ name: moodName });
+
+    if (existingOne) {
+        const newCount = existingOne.count + 1;
+        await collection.updateOne({ name: moodName },
+            { $set: { count: newCount } })
+    } else {
+        await collection.insertOne({ name: moodName, count: 1 });
+    }
+    res.status(201).send(`We have recorded that you are ${moodName}`)
+});
+
+app.listen(3000, () => {
+    console.log("I hear everything");
+});
