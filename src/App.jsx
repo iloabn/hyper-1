cbaf8700c7fec47a67810ec319f8b6f750a2e398
iloabn <5629868+iloabn@users.noreply.github.com>
2025-04-03 22:00:41 +0200

    Add template file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 function App() {
   return (
-    <>
-      <h2>Hehe</h2>
-    </>
+    <main>
+      <article>
+        <h2>A wonderful day</h2>
+        <p>Today is a really wonderful day. Just look at it!</p>
+        <img src="https://fabianmiiro.se/today.png" />
+      </article>
+      <article>
+        <h2>Sad computer is sad</h2>
+        <p>
+          Had to send in a computer for repairs. I hope it's an easy fix and
+          that nothing needs to be replaced.
+        </p>
+        <img src="https://fabianmiiro.se/sad_computer.png" />
+      </article>
+    </main>
   );
 }
 
