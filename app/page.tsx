f4ea5c5041f729124a89038305b3a2713a5383e8
iloabn <5629868+iloabn@users.noreply.github.com>
2025-04-03 22:00:11 +0200

    Add template file

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
 export default function Page() {
-  return <h1>Tihi!</h1>;
+  return (
+    <main>
+      <article>
+        <h2>A wonderful day</h2>
+        <p>Today is a really wonderful day. Just look at it!</p>
+        <img src="https://fabianmiiro.se/today.png" />
+      </article>
+      <article>
+        <h2>Sad computer is sad</h2>
+        <p>
+          Had to send in a computer for repairs. I hope it's an easy fix and
+          that nothing needs to be replaced.
+        </p>
+        <img src="https://fabianmiiro.se/sad_computer.png" />
+      </article>
+    </main>
+  );
 }
