b88181d97c23860238a8ddd2c470b18451b292da
iloabn <5629868+iloabn@users.noreply.github.com>
2025-04-04 10:32:18 +0200

    Step 2

---
+++ app/hello/page.tsx
@@ -0,0 +1,3 @@
+export default function Hello() {
+  return <h1 className="text-black">Hiiiiiiii</h1>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import "./globals.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-black">
+        {children}
+        <footer>The end</footer>
+      </body>
     </html>
   );
 }
